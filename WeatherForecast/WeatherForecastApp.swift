8060e8a22043629ff6c9e1bfb85f947c65c84fbf
edutim <timothy.hart@gmail.com>
2024-01-22 13:19:56 -0500

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Timothy Hart on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
